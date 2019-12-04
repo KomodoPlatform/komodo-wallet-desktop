437ed0945597f79143e76a5e12b32cee45d04d78
milerius <rmscastle@gmail.com>
2019-12-04 15:57:22 +0100

    feat(main_window): basis
    - Change Window title to AtomicDex
    - Add an invisible progress bar to the status bar for my background stuff
    - Add StatusBar contents structure.

--- atomic_dex_qt/mainwindow.cpp
+++ atomic_dex_qt/mainwindow.cpp
@@ -1,15 +1,17 @@
+
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+        : QMainWindow(parent), m_ui(new Ui::MainWindow),
+          m_status_bar_contents("loaded", new QProgressBar(statusBar())) {
+    m_ui->setupUi(this);
+    statusBar()->showMessage(m_status_bar_contents.default_msg);
+    statusBar()->addPermanentWidget(m_status_bar_contents.m_progress_bar);
+    m_status_bar_contents.m_progress_bar->setVisible(false);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() {
+    delete m_ui;
 }
 
--- atomic_dex_qt/mainwindow.h
+++ atomic_dex_qt/mainwindow.h
@@ -2,20 +2,34 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QProgressBar>
+#include <utility>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+struct StatusBarContents {
+    StatusBarContents(QString str, QProgressBar *in_progress_bar) noexcept : default_msg(std::move(str)),
+                                                                             m_progress_bar(in_progress_bar) {
+
+    }
+
+    QString default_msg{""};
+    QProgressBar *m_progress_bar{nullptr};
+};
+
+class MainWindow : public QMainWindow {
+Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
+
     ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow *m_ui;
+    StatusBarContents m_status_bar_contents;
 };
+
 #endif // MAINWINDOW_H
