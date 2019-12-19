1125e89efaf98427851bbb44dbfadc1add0ed7d5
milerius <rmscastle@gmail.com>
2019-12-19 22:43:33 +0100

    feat(nim): prettify

--- atomic_dex_desktop/src/atomic_dex_desktop/coins/coins_cfg.nim
+++ atomic_dex_desktop/src/atomic_dex_desktop/coins/coins_cfg.nim
@@ -9,43 +9,42 @@ import ../folly/hashmap
 var coins_registry: ConcurrentReg[string, JsonNode]
 
 jsonSchema:
-    ElectrumServerParams:
-        url: string
-        protocol ?: string
-        disable_cert_verification ?: bool
-    CoinConfigParams:
-        coin: string
-        asset ?: string
-        name: string
-        "type": string
-        rpcport: int
-        pubtype ?: int
-        p2shtype ?: int
-        wiftype ?: int
-        txversion ?: int
-        overwintered ?: int
-        txfee ?: int
-        mm2: int
-        coingecko_id: string
-        coinpaprika_id: string
-        is_erc_20: bool
-        electrum: ElectrumServerParams[]
-        explorer_url: string[]
-        active: bool
-        currently_enabled: bool
+  ElectrumServerParams:
+    url: string
+    protocol ?: string
+    disable_cert_verification ?: bool
+  CoinConfigParams:
+    coin: string
+    asset ?: string
+    name: string
+    "type": string
+    rpcport: int
+    pubtype ?: int
+    p2shtype ?: int
+    wiftype ?: int
+    txversion ?: int
+    overwintered ?: int
+    txfee ?: int
+    mm2: int
+    coingecko_id: string
+    coinpaprika_id: string
+    is_erc_20: bool
+    electrum: ElectrumServerParams[]
+    explorer_url: string[]
+    active: bool
+    currently_enabled: bool
 
 template whenValid*(data, kind, body) =
-    if data.isValid(kind):
-        var data = kind(data)
-        body
+  if data.isValid(kind):
+    var data = kind(data)
+    body
 
 proc parse_cfg*() =
-    let entire_file = readFile(get_assets_path() & "/config/coins.json")
-    let jsonNode = parseJson(entire_file)
-    for key in jsonNode.keys:
-      if jsonNode[key].isValid(CoinConfigParams):
-            assert(coins_registry.cm_insert_or_assign(key, jsonNode[key]).second == true, "should insert correctly")
-      else:
-            echo jsonNode[key], " is invalid"      
-    echo "Coins config correctly launched: ", coins_registry.cm_size()
-    
+  let entire_file = readFile(get_assets_path() & "/config/coins.json")
+  let jsonNode = parseJson(entire_file)
+  for key in jsonNode.keys:
+    if jsonNode[key].isValid(CoinConfigParams):
+      assert(coins_registry.cm_insert_or_assign(key, jsonNode[key]).second == true, "should insert correctly")
+    else:
+      echo jsonNode[key], " is invalid"
+  echo "Coins config correctly launched: ", coins_registry.cm_size()
