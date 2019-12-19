f9bf9284e025e48234e4841c8378ac30ca2534c7
milerius <rmscastle@gmail.com>
2019-12-19 09:54:11 +0100

    feat(nim): add loading font

--- atomic_dex_desktop/src/atomic_dex_desktop.nim
+++ atomic_dex_desktop/src/atomic_dex_desktop.nim
@@ -1,16 +1,16 @@
-import osproc
 import atomic_dex_desktop/utils/assets
 import atomic_dex_desktop/mm2/mm2
 import atomic_dex_desktop/gui/gui
 import ui_workflow_nim
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
 
 proc main() =
   mm2.init_process()
   defer: mm2.close_process()
-  var ctx = antara_ui_create("Hello", 200, 200)
+  var ctx = antara_ui_create("AtomicDex", 200, 200)
   defer: antara_ui_destroy(ctx)
+  echo get_assets_path() & "/fonts/Ruda-Bold.ttf"
+  antara_load_font(ctx, get_assets_path() & "/fonts/Ruda-Bold.ttf", 15.0)
+  antara_awesome_load_font(ctx, get_assets_path() & "/fonts/fa-solid-900.ttf", 16.0)
   gui.set_komodo_style()
   while antara_is_running(ctx) == 0:
     antara_pre_update(ctx)
@@ -19,4 +19,4 @@ proc main() =
     antara_update(ctx)
 
 when isMainModule:
-    main()
+  main()
