1fb4087a6eaf75840c3760d1cd6fd3fae1d3be45
romanszterg <rmscastle@gmail.com>
2020-01-07 19:37:17 +0100

    feat(qt): preparing qt transition

---
+++ atomic_qt/atomic_qt_en_US.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="8"/>
+        <source>Hello World</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
