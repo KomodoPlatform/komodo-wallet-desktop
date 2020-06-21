c7aa5d17d07844f350e5907f1b74044af8386964
romanszterg <rmscastle@gmail.com>
2020-06-21 14:33:21 +0200

    feat(unit_tests): prepare unit tests in the CI
    Signed-off-by: romanszterg <rmscastle@gmail.com>

---
+++ ci_tools_atomic_dex/src/tests.nim
@@ -0,0 +1,24 @@
+import os
+import osproc
+
+import build
+
+when defined(windows):
+    import generate
+
+proc run_tests*(build_type: string, osx_sdk_path: string, compiler_path: string) =
+     when defined(linux) or defined(osx):
+        build_atomic_qt(build_type, osx_sdk_path, compiler_path)
+     when defined(windows):
+        # build first and then generate to scan for missing dll
+        build_atomic_qt(build_type, osx_sdk_path, compiler_path)
+        generate_solution(build_type, osx_sdk_path, compiler_path)
+    
+     when defined(osx):
+        echo os.getCurrentDir()
+        os.setCurrentDir(os.getCurrentDir().joinPath("bin").joinPath("atomic_qt_tests.app").joinPath("Contents").joinPath("MacOS"))
+        echo "Running AtomicDex Pro Unit tests"
+        discard osproc.execCmd("./atomic_qt_tests --reporters=xml --out=atomic-dex-tests-result.xml -s")
+        echo "Successfully Generated atomic-dex-tests-result.xml"
+        
+        
