62f1ee0940d28f41110293b869873ca4d7790c05
romanszterg <rmscastle@gmail.com>
2020-07-01 16:14:03 +0200

    feat(candlestick chart): add integration test of the candlestick chart system
    Signed-off-by: romanszterg <rmscastle@gmail.com>

--- src/atomic.dex.provider.cex.prices.tests.cpp
+++ src/atomic.dex.provider.cex.prices.tests.cpp
@@ -14,6 +14,7 @@
  *                                                                            *
  ******************************************************************************/
 
+#include "atomic.dex.events.hpp"
 #include "atomic.dex.provider.cex.prices.hpp"
 #include <doctest/doctest.h>
 
@@ -21,6 +22,33 @@ TEST_CASE("atomic dex cex prices provider constructor")
 {
     entt::registry registry;
     registry.set<entt::dispatcher>();
-    atomic_dex::mm2 mm2(registry);
+    atomic_dex::mm2                 mm2(registry);
     atomic_dex::cex_prices_provider provider(registry, mm2);
+}
+
+SCENARIO("atomic dex cex price service functionnality")
+{
+    GIVEN("A basic environment")
+    {
+        entt::registry registry;
+        registry.set<entt::dispatcher>();
+        antara::gaming::ecs::system_manager system_manager_{registry};
+        auto&                               mm2_s      = system_manager_.create_system<atomic_dex::mm2>();
+        auto&                               cex_system = system_manager_.create_system<atomic_dex::cex_prices_provider>(mm2_s);
+
+        THEN("I start mm2")
+        {
+            registry.ctx<entt::dispatcher>().trigger<atomic_dex::mm2_started>();
+
+            AND_WHEN("i set the current orderbook pair to a valid supported pair (kmd-btc)")
+            {
+                registry.ctx<entt::dispatcher>().trigger<atomic_dex::orderbook_refresh>("kmd", "btc");
+                AND_THEN("i check if data are available, and if the port is not supported")
+                {
+                    CHECK(cex_system.is_ohlc_data_available());
+                    CHECK(cex_system.is_pair_supported("kmd", "btc"));
+                }
+            }
+        }
+    }
 }
