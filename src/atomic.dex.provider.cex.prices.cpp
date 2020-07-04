df761bd3641e6a2f605d6225a18d5c20a65585fb
romanszterg <rmscastle@gmail.com>
2020-07-04 14:53:05 +0200

    feat(candlestick): fix lowercase
    Signed-off-by: romanszterg <rmscastle@gmail.com>

--- src/atomic.dex.provider.cex.prices.cpp
+++ src/atomic.dex.provider.cex.prices.cpp
@@ -62,7 +62,7 @@ namespace atomic_dex
 
         {
             std::scoped_lock lock(m_orderbook_tickers_data_mutex);
-            m_current_orderbook_ticker_pair = {evt.base, evt.rel};
+            m_current_orderbook_ticker_pair = {boost::algorithm::to_lower_copy(evt.base), boost::algorithm::to_lower_copy(evt.rel)};
             spdlog::debug("new orderbook pair for cex provider [{} / {}]", m_current_orderbook_ticker_pair.first, m_current_orderbook_ticker_pair.second);
             auto [base, rel] = m_current_orderbook_ticker_pair;
             spawn([base = base, rel = rel, this]() { process_ohlc(base, rel); });
