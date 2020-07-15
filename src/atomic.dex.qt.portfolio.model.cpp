e7f5efdd17b36e406cfd478dbad2c7673c7545a4
romanszterg <rmscastle@gmail.com>
2020-07-15 18:49:50 +0200

    feat(portfolio): add name role in the switch
    Signed-off-by: romanszterg <rmscastle@gmail.com>

--- src/atomic.dex.qt.portfolio.model.cpp
+++ src/atomic.dex.qt.portfolio.model.cpp
@@ -29,7 +29,12 @@ namespace atomic_dex
         {
             beginInsertRows(QModelIndex(), this->m_model_data.count(), this->m_model_data.count() + 1);
             this->m_model_data.push_back(portfolio_data{
-                .ticker = "BTC", .name = "Bitcoin", .balance = "2", .main_currency_balance = "18400", .change_24h = "2.4", .main_currency_price_for_one_unit = "9200"});
+                .ticker                           = "BTC",
+                .name                             = "Bitcoin",
+                .balance                          = "2",
+                .main_currency_balance            = "18400",
+                .change_24h                       = "2.4",
+                .main_currency_price_for_one_unit = "9200"});
             endInsertRows();
         }
     }
@@ -61,6 +66,8 @@ namespace atomic_dex
             return item.change_24h;
         case MainCurrencyPriceForOneUnit:
             return item.main_currency_price_for_one_unit;
+        case NameRole:
+            return item.name;
         }
         return {};
     }
