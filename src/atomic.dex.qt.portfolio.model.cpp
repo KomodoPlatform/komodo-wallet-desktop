8c4ade6f317fc618adbf58c7d9f9ad0e4ea759d5
milerius <rmscastle@gmail.com>
2020-07-23 16:01:31 +0200

    feat(history): revert

--- src/atomic.dex.qt.portfolio.model.cpp
+++ src/atomic.dex.qt.portfolio.model.cpp
@@ -237,7 +237,6 @@ namespace atomic_dex
     int
     portfolio_model::get_length() const noexcept
     {
-        spdlog::trace("LENGTH -> {}", this->m_model_proxy->rowCount(QModelIndex()));
-        return this->m_model_proxy->rowCount(QModelIndex());
+        return this->rowCount(QModelIndex());
     }
 } // namespace atomic_dex
