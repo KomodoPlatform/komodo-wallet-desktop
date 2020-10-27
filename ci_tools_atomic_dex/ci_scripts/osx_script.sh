06229cc5066e26b52bfef76dc4e7acd5defc3a77
romanszterg <rmscastle@gmail.com>
2020-10-27 17:23:47 +0100

    feat(deps): full deps from kmd

--- ci_tools_atomic_dex/ci_scripts/osx_script.sh
+++ ci_tools_atomic_dex/ci_scripts/osx_script.sh
@@ -32,4 +32,4 @@ cd libwally-core
 sudo make -j3 install
 cd ..
 # get SDKs 
-git clone https://github.com/phracker/MacOSX-SDKs $HOME/sdk
+git clone https://github.com/KomodoPlatform/MacOSX-SDKs $HOME/sdk
