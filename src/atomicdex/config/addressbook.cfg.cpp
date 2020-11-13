f4bb8e3ac46642b771ad96f9a40424a72b58b0fb
syl <syleze777@gmail.com>
2020-11-13 17:27:11 +0100

    fix(addressbook): fix bad path when updating config and ensure file exists before loading config

--- src/atomicdex/config/addressbook.cfg.cpp
+++ src/atomicdex/config/addressbook.cfg.cpp
@@ -15,7 +15,7 @@
  ******************************************************************************/
  
 //! STD
-#include <fstream> //> std::ifstream, std::ofstream.
+#include <fstream> //> std::fstream, std::ofstream.
 
 //! Project Headers
 #include "addressbook.cfg.hpp"
@@ -25,29 +25,44 @@ namespace atomic_dex
 {
     nlohmann::json load_addressbook_cfg(const std::string& wallet_name)
     {
-        nlohmann::json out;
         fs::path       source_folder{utils::get_atomic_dex_addressbook_folder()};
         fs::path       in_path      {source_folder / wallet_name};
+        std::fstream   input;
+        nlohmann::json out;
         
         utils::create_if_doesnt_exist(source_folder);
         {
-            std::ifstream input{in_path.string()};
-            
+            //! Ensure file exists, if not, create it.
+            input.open(in_path.string(), std::ios::out);
             assert(input.is_open());
-            input >> out;
+            input.close();
+            assert(!input.is_open());
+            
+            //! Reads content
+            input.open(in_path.string(), std::ios::in);
+            assert(input.is_open());
+            try
+            {
+                input >> out;
+            }
+            catch ([[maybe_unused]] nlohmann::json::parse_error& ex)
+            {
+                spdlog::warn("Addressbook config file was invalid, its content will be cleaned.");
+                out = nlohmann::json::array();
+            }
             return out;
         }
     }
     
     void update_addressbook_cfg(const nlohmann::json& in, const std::string& wallet_name)
     {
-        fs::path out_folder{utils::get_atomic_dex_data_folder()};
-        fs::path out_path  {out_folder / wallet_name};
+        fs::path      out_folder{utils::get_atomic_dex_addressbook_folder()};
+        fs::path      out_path  {out_folder / wallet_name};
+        std::ofstream output;
         
         utils::create_if_doesnt_exist(out_path);
         {
-            std::ofstream output{out_path.string(), std::ios::trunc};
-            
+            output.open(out_path.string(), std::ios::trunc);
             assert(output.is_open());
             output << in;
         }
