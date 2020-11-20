b678d35ab7888ca29ede409012b25978d685deb2
roman sztergbaum <rmscastle@gmail.com>
2020-11-19 22:07:05 -0800

    better nim build

--- ci_tools_atomic_dex/src/bundle.nim
+++ ci_tools_atomic_dex/src/bundle.nim
@@ -3,13 +3,16 @@ import osproc
 
 import build
 import vcpkg
+import generate
 
 proc bundle*(build_type: string, osx_sdk_path: string, compiler_path: string) =
-    build_atomic_qt(build_type, osx_sdk_path, compiler_path)
+    generate_solution(build_type, osx_sdk_path, compiler_path)
+    #build_atomic_qt(build_type, osx_sdk_path, compiler_path)
     when defined(osx):
         discard osproc.execCmd("ninja install")
     when defined(windows):
-        discard osproc.execCmd("ninja install")
+        os.putEnv("NINJA_STATUS", "[%f/%t %e] ")
+        discard osproc.execCmd("ninja -v install")
     when defined(linux):
         discard osproc.execCmd("ninja install")
 
