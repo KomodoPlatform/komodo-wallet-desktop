9b5defb958000754a1fcee55497080fc4af4c4e3
romanszterg <rmscastle@gmail.com>
2021-01-15 16:10:22 +0100

    feat(internet): dont check komodo.live

--- src/atomicdex/services/internet/internet.checker.service.cpp
+++ src/atomicdex/services/internet/internet.checker.service.cpp
@@ -35,7 +35,7 @@ namespace
     }()};
     // t_http_client_ptr g_google_proxy_http_client{std::make_unique<web::http::client::http_client>(FROM_STD_STR("https://www.google.com"), g_cfg)};
     t_http_client_ptr g_paprika_proxy_http_client{std::make_unique<web::http::client::http_client>(FROM_STD_STR("https://api.coinpaprika.com"), g_cfg)};
-    t_http_client_ptr g_ohlc_proxy_http_client{std::make_unique<web::http::client::http_client>(FROM_STD_STR("https://komodo.live:3333"), g_cfg)};
+    //t_http_client_ptr g_ohlc_proxy_http_client{std::make_unique<web::http::client::http_client>(FROM_STD_STR("https://komodo.live:3333"), g_cfg)};
     // t_http_client_ptr g_cipig_proxy_http_client{std::make_unique<web::http::client::http_client>(FROM_STD_STR("https://dexapi.cipig.net:10000"), g_cfg)};
 
 
@@ -152,7 +152,7 @@ namespace atomic_dex
     {
         // query_internet(g_google_proxy_http_client, "", &internet_service_checker::is_google_reacheable);
         query_internet(g_paprika_proxy_http_client, "/v1/coins/btc-bitcoin", &internet_service_checker::is_paprika_provider_alive);
-        query_internet(g_ohlc_proxy_http_client, "/api/v1/ohlc/tickers_list", &internet_service_checker::is_our_private_endpoint_reacheable);
+        //query_internet(g_ohlc_proxy_http_client, "/api/v1/ohlc/tickers_list", &internet_service_checker::is_our_private_endpoint_reacheable);
         // query_internet(g_cipig_proxy_http_client, "", &internet_service_checker::is_cipig_electrum_alive);
     }
 } // namespace atomic_dex
