ba2bce29910339e357377fa4a8c2ed7d78ab1a9f
Roman Sztergbaum - Slyris - Milerius <rmscastle@gmail.com>
2021-02-23 06:15:27 +0100

    feat(windows): temporary disable for windows

--- src/atomicdex/pages/qt.wallet.page.cpp
+++ src/atomicdex/pages/qt.wallet.page.cpp
@@ -376,7 +376,7 @@ namespace atomic_dex
     void
     wallet_page::broadcast(const QString& tx_hex, bool is_claiming, bool is_max, const QString& amount) noexcept
     {
-#if defined(__APPLE__) || defined(WIN32)
+#if defined(__APPLE__)
         antara::gaming::core::evaluate_authentication(
             "Password to send funds is required", [=](bool is_auth) { broadcast_on_auth_finished(is_auth, tx_hex, is_claiming, is_max, amount); });
 #else
