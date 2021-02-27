ece6a1cccf8b669c1f836384135813f36582b552
syl <syleze777@gmail.com>
2021-02-27 20:42:46 +0100

    fix(cmake): change install icon name on macos

--- data/osx/dex_install.cmake
+++ data/osx/dex_install.cmake
@@ -2,7 +2,7 @@ if (APPLE)
     set_target_properties(${PROJECT_NAME} PROPERTIES
             MACOSX_BUNDLE_BUNDLE_NAME "${PROJECT_NAME}"
             RESOURCE data/osx/dex-logo.icns
-            MACOSX_BUNDLE_ICON_FILE ${PROJECT_NAME}
+            MACOSX_BUNDLE_ICON_FILE dex-logo.icns
             MACOSX_BUNDLE_SHORT_VERSION_STRING 0.0.1
             MACOSX_BUNDLE_LONG_VERSION_STRING 0.0.1
             MACOSX_BUNDLE_INFO_PLIST "${PROJECT_SOURCE_DIR}/cmake/MacOSXBundleInfo.plist.in")
