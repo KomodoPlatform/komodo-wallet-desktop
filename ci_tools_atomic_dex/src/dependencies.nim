ff1f676ac1ecd9215a233a73d9a466bdff4b17f0
romanszterg <rmscastle@gmail.com>
2021-03-03 08:57:44 +0100

    feat(ci): update to last vcpkg

--- ci_tools_atomic_dex/src/dependencies.nim
+++ ci_tools_atomic_dex/src/dependencies.nim
@@ -4,18 +4,4 @@ import vcpkg
 
 proc download_packages*() =
     echo "Downloading packages ... please wait"
-    #let response_file_directory = g_vcpkg_local_path.parentDir().parentDir().parentDir().joinPath(".github").joinPath("workflows")
-    #let custom_ports_path = g_vcpkg_local_path.parentDir().parentDir().joinPath("vcpkg-custom-ports").joinPath("ports")
-    #when defined(windows):
-    #    let cmd = g_vcpkg_local_path & " install @" & response_file_directory.joinPath("windows_response_file.txt") & " --overlay-ports=" & custom_ports_path
-    #    echo "vcpkg cmd [" & cmd & "]"
-    #    discard execCmd(cmd)
-    #when defined(linux):
-    #    let cmd = g_vcpkg_local_path & " install @" & response_file_directory.joinPath("linux_response_file.txt") & " --overlay-ports=" & custom_ports_path
-    #    echo "vcpkg cmd [" & cmd & "]"
-    #    discard execCmd(cmd)
-    #when defined(osx):
-    #    let cmd = g_vcpkg_local_path & " install @" & response_file_directory.joinPath("osx_response_file.txt") & " --overlay-ports=" & custom_ports_path
-    #    echo "vcpkg cmd [" & cmd & "]"
-    #    discard execCmd(cmd)
     echo "Downloading packages finished"
