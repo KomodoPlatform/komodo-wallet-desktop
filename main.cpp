c576e0b885c43c3f4b3a3a371459746ec7caa79a
Roman Sztergbaum <rmscastle@gmail.com>
2021-03-09 07:37:07 +0100

    feat(windows): when it's possible attach log to console

--- main.cpp
+++ main.cpp
@@ -9,14 +9,25 @@ main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 #endif
 {
 #if defined(WINDOWS_RELEASE_MAIN)
-    int    argc = __argc;
-    char** argv = __argv;
+    int    argc              = __argc;
+    char** argv              = __argv;
+    bool   is_parent_console = false;
     if (std::getenv("FORCE_ATOMICDEX_CONSOLE") != nullptr)
     {
-        AllocConsole();
-        freopen("conin$", "r", stdin);
-        freopen("conout$", "w", stdout);
-        freopen("conout$", "w", stderr);
+        if (is_parent_console = AttachConsole(ATTACH_PARENT_PROCESS); is_parent_console)
+        {
+            SPDLOG_INFO("Attaching console to the parent process");
+            freopen("conin$", "r", stdin);
+            freopen("conout$", "w", stdout);
+            freopen("conout$", "w", stderr);
+        }
+        else if (static_cast<bool>(AllocConsole()))
+        {
+            SPDLOG_INFO("Creating the console and redirecting output");
+            freopen("conin$", "r", stdin);
+            freopen("conout$", "w", stdout);
+            freopen("conout$", "w", stderr);
+        }
     }
 #endif
 
@@ -27,7 +38,10 @@ main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 #if defined(WINDOWS_RELEASE_MAIN)
     if (std::getenv("FORCE_ATOMICDEX_CONSOLE") != nullptr)
     {
-        system("pause");
+        if (!is_parent_console)
+        {
+            system("pause");
+        }
     }
 #endif
     return res;
