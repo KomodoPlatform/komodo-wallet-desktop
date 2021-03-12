451de9422ba5fc6a330ad69e9b22eed8d87d3a41
milerius <rmscastle@gmail.com>
2021-03-12 11:04:57 +0100

    feat(math): continue usage of safe_float

--- src/core/atomicdex/utilities/safe.float.cpp
+++ src/core/atomicdex/utilities/safe.float.cpp
@@ -2,6 +2,14 @@
 // Created by Sztergbaum Roman on 12/03/2021.
 //
 
+#if defined(linux) || defined(__APPLE__)
+#    define BOOST_STACKTRACE_USE_ADDR2LINE
+#    if defined(__APPLE__)
+#        define _GNU_SOURCE
+#    endif
+#    include <boost/stacktrace.hpp>
+#endif
+
 #include "atomicdex/utilities/safe.float.hpp"
 
 t_float_50
@@ -15,6 +23,9 @@ safe_float(const std::string& from) noexcept
     catch (const std::exception& error)
     {
         SPDLOG_ERROR("exception caught when creating a floating point number: {}", error.what());
+#if defined(linux) || defined(__APPLE__)
+        SPDLOG_ERROR("stacktrace: {}", boost::stacktrace::to_string(boost::stacktrace::stacktrace()));
+#endif
         return t_float_50(0);
     }
 }
