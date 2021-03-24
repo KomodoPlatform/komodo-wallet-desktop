1726554a8d10ea3e3e2c8f630cdca4806cffdb2f
milerius <rmscastle@gmail.com>
2021-03-24 08:19:46 +0100

    feat(bnb): add some genericity for custom coins addition

--- src/core/atomicdex/pages/qt.settings.page.cpp
+++ src/core/atomicdex/pages/qt.settings.page.cpp
@@ -378,11 +378,12 @@ namespace atomic_dex
                     &::mm2::api::g_qtum_proxy_http_client, "/contract/"s + contract_address.toStdString(), "QRC20"s, "QTUM"s, "QRC-20"s, "QTUM"s);
             case CoinTypeGadget::ERC20:
                 return std::make_tuple(
-                    &::mm2::api::g_etherscan_proxy_http_client, "/api/v1/erc_infos/"s + contract_address.toStdString(), "ERC20"s, "ETH"s, "ERC-20"s, "ETH"s);
+                    &::mm2::api::g_etherscan_proxy_http_client, "/api/v1/token_infos/erc20/"s + contract_address.toStdString(), "ERC20"s, "ETH"s, "ERC-20"s,
+                    "ETH"s);
             case CoinTypeGadget::BEP20:
                 return std::make_tuple(
-                    &::mm2::api::g_etherscan_proxy_http_client, "/api/v1/erc_infos/"s + contract_address.toStdString() + "&is_bep20=true", "BEP20"s, "ETH"s,
-                    "BEP-20"s, "BNB"s);
+                    &::mm2::api::g_etherscan_proxy_http_client, "/api/v1/token_infos/bep20/"s + contract_address.toStdString(), "BEP20"s, "ETH"s, "BEP-20"s,
+                    "BNB"s);
             default:
                 return std::make_tuple(&::mm2::api::g_etherscan_proxy_http_client, ""s, ""s, ""s, ""s, ""s);
             }
