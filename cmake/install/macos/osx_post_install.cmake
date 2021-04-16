de08119b8a844ebc40aa960ac99de8b58354a8cc
milerius <rmscastle@gmail.com>
2021-04-16 12:07:22 +0200

    feat(ci): disable notarization for osx gleec

--- cmake/install/macos/osx_post_install.cmake
+++ cmake/install/macos/osx_post_install.cmake
@@ -87,10 +87,10 @@ if (NOT EXISTS ${CMAKE_SOURCE_DIR}/bin/${DEX_PROJECT_NAME}.dmg)
             ECHO_OUTPUT_VARIABLE
             ECHO_ERROR_VARIABLE)
 
-    execute_process(COMMAND ${PROJECT_ROOT_DIR}/cmake/install/macos/macos_notarize.sh --app-specific-password=$ENV{APPLE_ATOMICDEX_PASSWORD} --apple-id=$ENV{APPLE_ID} --primary-bundle-id=com.komodoplatform.atomicdex --target-binary=${CMAKE_SOURCE_DIR}/bin/${DEX_PROJECT_NAME}.dmg
-            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-            ECHO_OUTPUT_VARIABLE
-            ECHO_ERROR_VARIABLE)
+    #execute_process(COMMAND ${PROJECT_ROOT_DIR}/cmake/install/macos/macos_notarize.sh --app-specific-password=$ENV{APPLE_ATOMICDEX_PASSWORD} --apple-id=$ENV{APPLE_ID} --primary-bundle-id=com.komodoplatform.atomicdex --target-binary=${CMAKE_SOURCE_DIR}/bin/${DEX_PROJECT_NAME}.dmg
+    #        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    #        ECHO_OUTPUT_VARIABLE
+    #        ECHO_ERROR_VARIABLE)
 else()
     message(STATUS "dmg already generated - skipping")
 endif ()
@@ -123,10 +123,10 @@ execute_process(COMMAND codesign --deep --force -v -s "$ENV{MAC_SIGN_IDENTITY}" 
         ECHO_OUTPUT_VARIABLE
         ECHO_ERROR_VARIABLE)
 
-execute_process(COMMAND ${PROJECT_ROOT_DIR}/cmake/install/macos/macos_notarize.sh --app-specific-password=$ENV{APPLE_ATOMICDEX_PASSWORD} --apple-id=$ENV{APPLE_ID} --primary-bundle-id=com.komodoplatform.atomicdex --target-binary=${PROJECT_ROOT_DIR}/ci_tools_atomic_dex/installer/osx/${DEX_PROJECT_NAME}_installer.app
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-        ECHO_OUTPUT_VARIABLE
-        ECHO_ERROR_VARIABLE)
+#execute_process(COMMAND ${PROJECT_ROOT_DIR}/cmake/install/macos/macos_notarize.sh --app-specific-password=$ENV{APPLE_ATOMICDEX_PASSWORD} --apple-id=$ENV{APPLE_ID} --primary-bundle-id=com.komodoplatform.atomicdex --target-binary=${PROJECT_ROOT_DIR}/ci_tools_atomic_dex/installer/osx/${DEX_PROJECT_NAME}_installer.app
+#        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+#        ECHO_OUTPUT_VARIABLE
+#        ECHO_ERROR_VARIABLE)
 
 file(COPY ${PROJECT_ROOT_DIR}/ci_tools_atomic_dex/installer/osx/${DEX_PROJECT_NAME}_installer.app DESTINATION ${TARGET_APP_PATH})
 
