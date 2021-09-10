4cc278b0c585351483e493fe7c1588c03e398eed
milerius <rmscastle@gmail.com>
2021-09-10 08:35:17 +0200

    feat(price_service): continue the komodo price provider implementation

--- src/core/atomicdex/services/price/komodo_prices/komodo.prices.provider.cpp
+++ src/core/atomicdex/services/price/komodo_prices/komodo.prices.provider.cpp
@@ -1,2 +1,21 @@
 //! Project Headers
 #include "atomicdex/services/price/komodo_prices/komodo.prices.provider.hpp"
+
+//! Constructor
+namespace atomic_dex
+{
+    komodo_prices_provider::komodo_prices_provider(entt::registry& registry) : system(registry)
+    {
+        SPDLOG_INFO("komodo_prices_provider created");
+        this->disable();
+    }
+} // namespace atomic_dex
+
+//! Functions
+namespace atomic_dex
+{
+    void
+    komodo_prices_provider::update()
+    {
+    }
+} // namespace atomic_dex
--- src/core/atomicdex/services/price/komodo_prices/komodo.prices.provider.hpp
+++ src/core/atomicdex/services/price/komodo_prices/komodo.prices.provider.hpp
@@ -1,1 +1,35 @@
 #pragma once
+
+//! STD
+#include <shared_mutex>
+
+//! Deps
+#include <antara/gaming/ecs/system.manager.hpp>
+
+//! Project Headers
+#include "atomicdex/api/komodo_prices/komodo.prices.hpp"
+
+namespace atomic_dex
+{
+    class komodo_prices_provider final : public ag::ecs::pre_update_system<komodo_prices_provider>
+    {
+        //! private type definition
+        using t_market_registry = atomicdex::komodo_prices::api::t_komodo_tickers_price_registry;
+
+        //! private fields
+        t_market_registry         m_market_registry;
+        mutable std::shared_mutex m_market_mutex;
+
+      public:
+        //! Constructor
+        komodo_prices_provider(entt::registry& registry);
+
+        //! Destructor
+        ~komodo_prices_provider() final = default;
+
+        //! Override ag::system functions
+        void update() final;
+    };
+}
+
+REFL_AUTO(type(atomic_dex::komodo_prices_provider))
