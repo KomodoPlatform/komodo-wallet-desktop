d78dfc96dd57d1ea3b6eeb040d6895604729f62b
Roman Sztergbaum <rmscastle@gmail.com>
2021-12-10 06:42:44 +0100

    feat(windows): fix installer

--- ci_tools_atomic_dex/ci_scripts/mt_wrapper.ps1
+++ ci_tools_atomic_dex/ci_scripts/mt_wrapper.ps1
@@ -1,5 +1,16 @@
 $input=$args[0]
 $output=$args[1]
 
-echo $input $output
-mt.exe -manifest $input -outputresource:$output;#1
+
+$path=${Env:ProgramFiles(x86)}
+Get-ChildItem $path -recurse -include "mt.exe" | Foreach-Object { 
+    if($_.FullName -like '*x86\*') 
+    { 
+        echo $_.FullName
+        $target=(get-item $_.FullName).Directory.FullName
+        echo "Adding to path: $target"
+        $env:Path += ";$target"
+    }
+}
+
+mt.exe -manifest $input -outputresource:$output;
