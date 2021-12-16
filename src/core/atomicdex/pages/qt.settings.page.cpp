b3713d02806d93b0208e1f833b63795715bbba93
Sztergbaum <rmscastle@gmail.com>
2021-12-16 18:35:51 +0100

    feat(raw_cfg): add chain_id

--- src/core/atomicdex/pages/qt.settings.page.cpp
+++ src/core/atomicdex/pages/qt.settings.page.cpp
@@ -431,14 +431,6 @@ namespace atomic_dex
             out["mm2_cfg"]      = nlohmann::json::object();
             out["adex_cfg"]     = nlohmann::json::object();
             const auto& mm2     = this->m_system_manager.get_system<mm2_service>();
-            if (parent_chain == "BNB")
-            {
-                out["mm2_cfg"]["chain_id"] = 56;
-            }
-            else if (parent_chain == "ETH")
-            {
-                out["mm2_cfg"]["chain_id"] = 1;
-            }
 
             if (resp.status_code() == 200)
             {
@@ -447,8 +439,8 @@ namespace atomic_dex
                 const auto     ticker         = body_json.at("symbol").get<std::string>() + "-" + type;
                 const auto     name_lowercase = body_json.at("tokenName").get<std::string>();
 
-                out["ticker"]                 = ticker;
-                out["name"]                   = name_lowercase;
+                out["ticker"] = ticker;
+                out["name"]   = name_lowercase;
                 copy_icon(icon_filepath, get_custom_coins_icons_path(), atomic_dex::utils::retrieve_main_ticker(ticker));
                 if (not is_this_ticker_present_in_raw_cfg(QString::fromStdString(ticker)))
                 {
@@ -466,8 +458,11 @@ namespace atomic_dex
                     out["mm2_cfg"]["decimals"]                                      = std::stoi(body_json.at("divisor").get<std::string>());
                     out["mm2_cfg"]["avg_blocktime"]                                 = raw_parent_cfg.at("avg_blocktime");
                     out["mm2_cfg"]["required_confirmations"]                        = raw_parent_cfg.at("required_confirmations");
-                    out["mm2_cfg"]["name"]                                          = name_lowercase;
-                    
+                    if (raw_parent_cfg.contains("chain_id"))
+                    {
+                        out["mm2_cfg"]["chain_id"] = raw_parent_cfg.at("chain_id");
+                    }
+                    out["mm2_cfg"]["name"] = name_lowercase;
                 }
                 if (not is_this_ticker_present_in_normal_cfg(QString::fromStdString(ticker)))
                 {
@@ -670,8 +665,8 @@ namespace atomic_dex
                         auto       show_priv_key_answer = ::mm2::api::rpc_process_answer_batch<::mm2::api::show_priv_key_answer>(answer, "show_priv_key");
                         auto*      portfolio_mdl        = this->m_system_manager.get_system<portfolio_page>().get_portfolio();
                         const auto idx                  = portfolio_mdl->match(
-                            portfolio_mdl->index(0, 0), portfolio_model::TickerRole, QString::fromStdString(show_priv_key_answer.coin), 1,
-                            Qt::MatchFlag::MatchExactly);
+                                             portfolio_mdl->index(0, 0), portfolio_model::TickerRole, QString::fromStdString(show_priv_key_answer.coin), 1,
+                                             Qt::MatchFlag::MatchExactly);
                         if (not idx.empty())
                         {
                             update_value(portfolio_model::PrivKey, QString::fromStdString(show_priv_key_answer.priv_key), idx.at(0), *portfolio_mdl);
