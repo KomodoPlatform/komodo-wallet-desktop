9f514d10616239f88af5b6f7ef119bab2eae4388
smk762 <35845239+smk762@users.noreply.github.com>
2021-12-20 20:19:02 +0800

    add fallback for tar

--- ci_tools_atomic_dex/ci_scripts/linux_script.sh
+++ ci_tools_atomic_dex/ci_scripts/linux_script.sh
@@ -62,8 +62,7 @@ sudo make -j3 install
 cd ..
 
 ## tar
-wget https://ftp.gnu.org/gnu/tar/tar-1.32.tar.gz
-tar xvf tar-1.32.tar.gz
+time wget --timeout=10 --tries=3 https://ftp.gnu.org/gnu/tar/tar-1.32.tar.gz || wget --timeout=10 --tries=3 https://mirrors.sjtug.sjtu.edu.cn/gnu/tar/tar-1.32.tar.gztar xvf tar-1.32.tar.gz
 cd tar-1.32
 export FORCE_UNSAFE_CONFIGURE=1
 ./configure
