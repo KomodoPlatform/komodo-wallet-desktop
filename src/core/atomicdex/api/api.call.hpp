13fd9dee28cfb6ad6867556c29718885049c2aef
Syl <syleze777@gmail.com>
2022-02-14 01:04:14 +0100

    Add api call concept

---
+++ src/core/atomicdex/api/api.call.hpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+ * Copyright © 2013-2022 The Komodo Platform Developers.                      *
+ *                                                                            *
+ * See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at                  *
+ * the top-level directory of this distribution for the individual copyright  *
+ * holder information and the developer policies on copyright and licensing.  *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement, no part of the    *
+ * Komodo Platform software, including this file may be copied, modified,     *
+ * propagated or distributed except according to the terms contained in the   *
+ * LICENSE file                                                               *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+namespace atomic_dex
+{
+    template <typename T>
+    concept api_call = requires(T t)
+    {
+        T::endpoint || t.endpoint;
+        typename T::expected_request_type; t.request;
+        typename T::expected_answer_type; t.answer;
+    };
+}
