5e66a75dad21a3c08022b677e176a7a4e7844f00
smk762 <35845239+smk762@users.noreply.github.com>
2022-03-25 14:01:42 +0800

    use KP fork

--- ci_tools_atomic_dex/ci_scripts/linux_script.sh
+++ ci_tools_atomic_dex/ci_scripts/linux_script.sh
@@ -54,7 +54,7 @@ sudo cp -r * /usr/local/
 cmake --version
 
 # get libwally
-git clone --branch test https://github.com/smk762/libwally-core.git --recursive
+git clone --branch update https://github.com/KomodoPlatform/libwally-core --recursive
 cd libwally-core
 ./tools/autogen.sh
 ./configure --disable-shared
