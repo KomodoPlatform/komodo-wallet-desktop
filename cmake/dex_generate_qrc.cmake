2d91e5287cc8093e449c42c4e48046e7b090f5cd
syl <syleze777@gmail.com>
2022-04-07 19:08:35 +0200

    CMake: add function to generate qrc files

---
+++ cmake/dex_generate_qrc.cmake
@@ -0,0 +1,24 @@
+function(dex_generate_qrc directory output)
+    cmake_parse_arguments(
+        GENERATE_QRC
+        ""
+        "OUTPUT"
+        ""
+        ${ARGN}
+    )
+
+    set(resources)
+    file(GLOB_RECURSE resources ${directory}/* ${directory}/*/*)
+
+    file(WRITE ${directory}/qml.qrc "<RCC>\n")
+    file(APPEND ${directory}/qml.qrc "    <qresource prefix=\"/\">\n")
+    foreach(res ${resources})
+        string(REPLACE ${directory}/ "" res ${res})
+        file(APPEND ${directory}/qml.qrc "        <file>${res}</file>\n")
+    endforeach()
+    file(APPEND ${directory}/qml.qrc "    </qresource>\n")
+    file(APPEND ${directory}/qml.qrc "</RCC>\n")
+
+    set(output ${directory}/qml.qrc PARENT_SCOPE)
+
+endfunction()
