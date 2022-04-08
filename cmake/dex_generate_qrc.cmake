ef2c38c8660e36618be604c9634789ddd427b381
syl <syleze777@gmail.com>
2022-04-08 15:27:23 +0200

    CMake: update function to generate qrc

--- cmake/dex_generate_qrc.cmake
+++ cmake/dex_generate_qrc.cmake
@@ -1,9 +1,17 @@
 function(dex_generate_qrc directory output)
+    cmake_parse_arguments(
+        GENERATE_QRC
+        ""
+        "PATH_PREFIX"
+        ""
+        ${ARGN}
+    )
+
     set(resources)
     file(GLOB_RECURSE resources ${directory}/* ${directory}/*/*)
 
     file(WRITE ${directory}/qml.qrc "<RCC>\n")
-    file(APPEND ${directory}/qml.qrc "    <qresource prefix=\"/\">\n")
+    file(APPEND ${directory}/qml.qrc "    <qresource prefix=\"/${GENERATE_QRC_PATH_PREFIX}\">\n")
     foreach(res ${resources})
         string(REPLACE ${directory}/ "" res ${res})
         file(APPEND ${directory}/qml.qrc "        <file>${res}</file>\n")
