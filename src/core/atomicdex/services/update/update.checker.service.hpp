6c37a597ba758a7e17f0f3aa4611e12e9e09b7f9
syl <syleze777@gmail.com>
2022-05-27 13:20:50 +0200

    Refactor update checker service

--- src/core/atomicdex/services/update/update.checker.service.hpp
+++ src/core/atomicdex/services/update/update.checker.service.hpp
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright © 2013-2021 The Komodo Platform Developers.                      *
+ * Copyright © 2013-2022 The Komodo Platform Developers.                      *
  *                                                                            *
  * See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at                  *
  * the top-level directory of this distribution for the individual copyright  *
@@ -16,53 +16,41 @@
 
 #pragma once
 
-//! Qt
 #include <QObject>
 #include <QVariant>
 
-//! Deps
 #include <boost/thread/synchronized_value.hpp>
 #include <nlohmann/json_fwd.hpp>
 
-//! Project Headers
 #include <antara/gaming/ecs/system.hpp>
 
 namespace atomic_dex
 {
-    class update_service_checker final : public QObject, public ag::ecs::pre_update_system<update_service_checker>
+    class update_checker_service final : public QObject, public ag::ecs::pre_update_system<update_checker_service>
     {
         Q_OBJECT
 
-        Q_PROPERTY(QVariant update_status READ get_update_status NOTIFY updateStatusChanged)
+        Q_PROPERTY(QVariant updateInfo READ get_update_info NOTIFY updateInfoChanged)
 
-        //! Private typedefs
         using t_update_time_point = std::chrono::high_resolution_clock::time_point;
         using t_json_synchronized = boost::synchronized_value<nlohmann::json>;
 
-        //! Private members
-        t_json_synchronized m_update_status;
+        t_json_synchronized m_update_info;
         t_update_time_point m_update_clock;
 
-        //! Private API
-        void fetch_update_status() ;
-
-      signals:
-        void updateStatusChanged();
+        void fetch_update_info();
 
       public:
-        [[deprecated("Use self_update_service instead.")]]
-        //! Constructor
-        explicit update_service_checker(entt::registry& registry, QObject* parent = nullptr);
+        explicit update_checker_service(entt::registry& registry, QObject* parent = nullptr);
+        ~update_checker_service() final = default;
 
-        //! Destructor
-        ~update_service_checker()  final = default;
+        void update() final;
 
-        //! Public override
-        void update()  final;
+        [[nodiscard]] QVariant get_update_info() const;
 
-        //! Properties
-        [[nodiscard]] QVariant get_update_status() const ;
+      signals:
+        void updateInfoChanged();
     };
 } // namespace atomic_dex
 
-REFL_AUTO(type(atomic_dex::update_service_checker))
+REFL_AUTO(type(atomic_dex::update_checker_service))
