f2d2f968a2d853a3c874c07ed19ef7e284620ddc
smk762 <smk762@iinet.net.au>
2022-08-05 18:46:12 +0800

    Pepe-fied

--- ci_tools_atomic_dex/util/compare_themes.py
+++ ci_tools_atomic_dex/util/compare_themes.py
@@ -2,45 +2,83 @@
 import json
 import requests
 
-branches = ['dev', 'smartdex', 'GleecDEX', 'shibadex']
+'''
+Purpose: Scans the light and dark theme json files for a list of whitelabel
+         branches, and compares them with the dev branch to identify theme
+         selectors which are obsolete or not present in the whitelabel branch.
 
-themes = {}
-for branch in branches:
-    themes.update({branch: {}})
-    for theme in ['light', 'dark']:
-        url = f"https://raw.githubusercontent.com/KomodoPlatform/atomicDEX-Desktop/{branch}/assets/themes/Default%20-%20{theme.title()}/colors.json"
-        themes[branch].update({
-            theme: requests.get(url).json()
-        })
+Usage:   `./compare_themes.py`
+'''
 
-dev_selectors = {
-    'light': set(themes['dev']['light'].keys()),
-    'dark': set(themes['dev']['dark'].keys())
-}
+REPO_URL = "https://raw.githubusercontent.com/KomodoPlatform/atomicDEX-Desktop"
+BRANCHES = ['dev', 'smartdex', 'GleecDEX', 'shibadex']
 
-for branch in branches:
-    if branch != "dev":
+
+def get_theme_url(branch, theme):
+    '''Returns a github url for a branch theme.'''
+    path = f"assets/themes/Default%20-%20{theme.title()}"
+    return f"{REPO_URL}/{branch}/{path}/colors.json"
+
+
+def get_themes_data(branches):
+    '''Returns a dict of dark/light theme data for each branch.'''
+    themes = {}
+    for branch in branches:
+        themes.update({branch: {}})
         for theme in ['light', 'dark']:
-            selectors = set(themes[branch][theme].keys())
-            missing = dev_selectors[theme].difference(selectors)
+            url = get_theme_url(branch, theme)
             themes[branch].update({
-                f"missing_{theme}": missing,
-                f"obsolete_{theme}": selectors.difference(dev_selectors[theme])
+                theme: requests.get(url).json()
             })
-
-            print(f"\n#### {branch} {theme} ####")
-            for i in themes[branch][f"obsolete_{theme}"]:
-                print(f"Obsolete selector: {i}...")
-
-            for i in themes[branch][f"missing_{theme}"]:
-
-                dev_color = themes['dev'][theme][i]
-                for j in themes['dev'][theme]:
-                    if dev_color == themes['dev'][theme][j]:
-                        if j in themes[branch][theme]:
-                            print(f"Missing {i}... Suggest using {themes[branch][theme][j]}")
-                            break
-                
+    return themes
 
 
+def get_selectors(themes, branch='dev'):
+    '''Returns a list of selectors within each theme for a branch.'''
+    return {
+        'light': set(themes[branch]['light'].keys()),
+        'dark': set(themes[branch]['dark'].keys())
+    }
 
+
+def compare_branch_themes(branches, show_results=True):
+    '''Scans whitelabel theme data to identify missing/obsolete selectors.'''
+    themes = get_themes_data(branches)
+    selectors = get_selectors(themes, 'dev')
+
+    for branch in branches:
+        if branch != "dev":
+            for theme in ['light', 'dark']:
+                selectors = set(themes[branch][theme].keys())
+                missing = selectors[theme].difference(selectors)
+                themes[branch].update({
+                    f"missing_{theme}": missing,
+                    f"obsolete_{theme}": selectors.difference(selectors[theme])
+                })
+                if show_results:
+                    output_results(themes, branch, theme)
+
+
+def output_results(themes, branch, theme):
+    '''Outputs results for a branch to the console.'''
+    print(f"\n#### {branch} {theme} ####")
+    if len(themes[branch][f"obsolete_{theme}"]) == 0:
+        print(f"No obsolete selectors")
+    else:
+        for i in themes[branch][f"obsolete_{theme}"]:
+            print(f"Obsolete selector: {i}...")
+
+    if len(themes[branch][f"missing_{theme}"]) == 0:
+        print(f"No obsolete selectors")
+    else:
+        for i in themes[branch][f"missing_{theme}"]:
+            dev_color = themes['dev'][theme][i]
+            for j in themes['dev'][theme]:
+                if dev_color == themes['dev'][theme][j]:
+                    if j in themes[branch][theme]:
+                        print(f"Missing {i}... Try {themes[branch][theme][j]}")
+                        break
+
+
+if __name__ == '__main__':
+    compare_branch_themes(BRANCHES, True)
