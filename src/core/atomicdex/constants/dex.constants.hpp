a3b024c2ecacd2f7dd4b2d6818e5a31f8ed270db
smk762 <smk762@iinet.net.au>
2022-08-05 13:38:35 +0800

    use char for DEX_RPC

--- src/core/atomicdex/constants/dex.constants.hpp
+++ src/core/atomicdex/constants/dex.constants.hpp
@@ -3,7 +3,7 @@
 namespace atomic_dex
 {
     inline const char*       g_dex_api{DEX_API};
-    inline const std::string g_dex_rpc{DEX_RPC};
+    inline const char*       g_dex_rpc{DEX_RPC};
     inline const int64_t     g_dex_rpcport{std::stoi(DEX_RPCPORT)};
     inline const std::string g_primary_dex_coin{DEX_PRIMARY_COIN};
     inline const std::string g_second_primary_dex_coin{DEX_SECOND_PRIMARY_COIN};
