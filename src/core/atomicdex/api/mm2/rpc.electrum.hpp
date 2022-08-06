451928612be43abe7912c1897e42e04fee8bd2d9
smk762 <smk762@iinet.net.au>
2022-08-07 02:44:36 +0800

    add SLP related configs

--- src/core/atomicdex/api/mm2/rpc.electrum.hpp
+++ src/core/atomicdex/api/mm2/rpc.electrum.hpp
@@ -37,6 +37,8 @@ namespace mm2::api
         const std::string                        mainnet_qrc_swap_contract_address{"0x2f754733acd6d753731c00fee32cb484551cc15d"};
         const std::string                        mainnet_fallback_qrc_swap_contract_address{mainnet_qrc_swap_contract_address};
         std::optional<nlohmann::json>            address_format;
+        std::optional<std::vector<std::string>>  bchd_urls;
+        std::optional<bool>                      allow_slp_unsafe_conf;
     };
 
     struct electrum_answer
--- src/core/atomicdex/models/qt.global.coins.cfg.model.cpp
+++ src/core/atomicdex/models/qt.global.coins.cfg.model.cpp
@@ -324,6 +324,19 @@ namespace atomic_dex
         return m_proxies[CoinType::UTXO];
     }
 
+
+    global_coins_cfg_proxy_model*
+    global_coins_cfg_model::get_all_slp_proxy() const
+    {
+        return m_proxies[CoinType::SLP];
+    }
+
+    global_coins_cfg_proxy_model*
+    global_coins_cfg_model::get_all_zhtlc_proxy() const
+    {
+        return m_proxies[CoinType::ZHTLC];
+    }
+
     int
     global_coins_cfg_model::get_length() const 
     {
