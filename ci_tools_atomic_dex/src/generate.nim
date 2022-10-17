1eab37416add97c89c32121f93a7b779c70116f4
smk762 <smk762@iinet.net.au>
2022-10-18 00:26:22 +0800

    nim syntax

--- ci_tools_atomic_dex/src/generate.nim
+++ ci_tools_atomic_dex/src/generate.nim
@@ -6,7 +6,7 @@ import dependencies
 proc generate_solution*(build_type: string, osx_sdk_path: string, compiler_path: string) =
     download_packages()
     var full_name = "build-" & build_type 
-    if not os.existsDir(${PROJECT_ROOT_DIR}.joinPath(full_name)):
+    if not os.existsDir(os.getEnv(PROJECT_ROOT_DIR).joinPath(full_name)):
         os.createDir(full_name)
         echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
         echo os.getCurrentDir()
@@ -18,9 +18,9 @@ proc generate_solution*(build_type: string, osx_sdk_path: string, compiler_path:
         echo "existing directory: " & full_name
         echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
-    os.setCurrentDir(${PROJECT_ROOT_DIR}.joinPath(full_name))
+    os.setCurrentDir(os.getEnv(PROJECT_ROOT_DIR).joinPath(full_name))
     assert(os.existsEnv("QT_INSTALL_CMAKE_PATH"))
-    var cmd_line = "cmake -GNinja -DCMAKE_BUILD_TYPE=" & build_type & " " & ${PROJECT_ROOT_DIR}
+    var cmd_line = "cmake -GNinja -DCMAKE_BUILD_TYPE=" & build_type & " " & os.getEnv(PROJECT_ROOT_DIR)
     when defined(osx):
         if not osx_sdk_path.isNil() and osx_sdk_path != "nil":
             cmd_line = cmd_line & " -DCMAKE_OSX_SYSROOT=" & osx_sdk_path & " -DCMAKE_OSX_DEPLOYMENT_TARGET=10.14 -DPREFER_BOOST_FILESYSTEM=ON"
