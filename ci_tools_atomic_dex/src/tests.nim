0ef1a975bb026e36cecc15a1b8a93ea16570a4b3
smk762 <smk762@iinet.net.au>
2022-10-18 19:32:05 +0800

    remove logs

--- ci_tools_atomic_dex/src/tests.nim
+++ ci_tools_atomic_dex/src/tests.nim
@@ -14,29 +14,19 @@ proc run_tests*(build_type: string, osx_sdk_path: string, compiler_path: string)
      var full_name = "build-" & build_type
 
      when defined(osx):
-        echo os.getCurrentDir()
-        discard osproc.execCmd("ls")
         os.setCurrentDir(os.getEnv("PROJECT_ROOT_DIR").joinPath(full_name).joinPath("bin").joinPath(os.getEnv("DEX_PROJECT_NAME") & "_tests.app").joinPath("Contents").joinPath("MacOS"))
-        echo os.getCurrentDir()
-        discard osproc.execCmd("ls")
         echo "Running AtomicDex Pro Unit tests"
         discard osproc.execCmd("./" & os.getEnv("DEX_PROJECT_NAME") & "_tests --reporters=xml --out=" & os.getEnv("DEX_PROJECT_NAME") & "-tests-result.xml -s")
         echo "Successfully Generated ", os.getEnv("DEX_PROJECT_NAME"), "-tests-result.xml"
    
      when defined(linux):
-        echo os.getCurrentDir()
-        discard osproc.execCmd("ls")
         os.setCurrentDir(os.getEnv("PROJECT_ROOT_DIR").joinPath(full_name).joinPath("bin").joinPath("AntaraAtomicDexTestsAppDir").joinPath("usr").joinPath("bin"))
-        echo os.getCurrentDir()
-        discard osproc.execCmd("ls")
         echo "Running AtomicDex Pro Unit tests"
         discard osproc.execCmd("./" & os.getEnv("DEX_PROJECT_NAME") & "_tests --reporters=xml --out=" & os.getEnv("DEX_PROJECT_NAME") & "-tests-result.xml -s")
         echo "Successfully Generated ", os.getEnv("DEX_PROJECT_NAME"), "-tests-result.xml"
      
      when defined(windows):
-        echo os.getCurrentDir()
         os.setCurrentDir(os.getEnv("PROJECT_ROOT_DIR").joinPath(full_name).joinPath("bin"))
-        echo os.getCurrentDir()
         echo "Running AtomicDex Pro Unit tests"
         discard osproc.execCmd(".\\" & os.getEnv("DEX_PROJECT_NAME") & "_tests --reporters=xml --out=" & os.getEnv("DEX_PROJECT_NAME") & "-tests-result.xml -s")
         echo "Successfully Generated ", os.getEnv("DEX_PROJECT_NAME"), "-tests-result.xml"
