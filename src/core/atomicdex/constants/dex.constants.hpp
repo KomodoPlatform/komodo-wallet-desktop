720660aad68a82bc8173c83c827ca695dffb811c
smk762 <smk762@iinet.net.au>
2022-11-14 03:07:39 +0800

    allow for more default coins - test with rick and morty

--- src/core/atomicdex/constants/dex.constants.hpp
+++ src/core/atomicdex/constants/dex.constants.hpp
@@ -8,7 +8,13 @@ namespace atomic_dex
     inline const std::string g_primary_dex_coin{DEX_PRIMARY_COIN};
     inline const std::string g_second_primary_dex_coin{DEX_SECOND_PRIMARY_COIN};
     inline const std::string g_third_primary_dex_coin{DEX_THIRD_PRIMARY_COIN};
-    inline const std::vector<std::string> g_default_coins{g_primary_dex_coin, g_second_primary_dex_coin, g_third_primary_dex_coin};
+    inline const std::vector<std::string> g_default_coins{
+        g_primary_dex_coin,
+        g_second_primary_dex_coin,
+        g_third_primary_dex_coin,
+        "RICK",
+        "MORTY"
+    };
     inline const std::vector<std::string> g_wallet_only_coins{
         "ARRR-BEP20",
         "RBTC",
