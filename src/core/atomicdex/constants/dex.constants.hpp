7ddfefbea27e38d88e78f62b3878f4c4deebd4c1
smk762 <smk762@iinet.net.au>
2022-11-25 01:17:24 +0800

    add default coins

--- src/core/atomicdex/constants/dex.constants.hpp
+++ src/core/atomicdex/constants/dex.constants.hpp
@@ -11,14 +11,24 @@ namespace atomic_dex
         g_primary_dex_coin,
         g_second_primary_dex_coin,
         "BABYDOGE-BEP20",
+        "BANANO-BEP20",
         "BNB",
         "BONE-ERC20",
         "BTC",
+        "CAKE",
+        "CUMMIES-BEP20",
+        "DOGE-BEP20",
+        "DOGGY-BEP20",
         "DOGEDASH-BEP20",
         "ETH",
+        "FLOKI-BEP20",
+        "GM-BEP20",
         "LEASH-ERC20",
+        "MONA",
         "SHIB-BEP20",
-        "TAMA-BEP20"
+        "TAMA-BEP20",
+        "SHIB-ERC20",
+        "ZINU-BEP20"
     };
     inline const std::vector<std::string> g_wallet_only_coins{
         "ARRR-BEP20",
