7aab286eaef4985a481864a0869c1ce50bf555ce
smk762 <smk762@iinet.net.au>
2023-01-02 17:09:21 +0800

    try latest nim

--- ci_tools_atomic_dex/ci_scripts/osx_script.sh
+++ ci_tools_atomic_dex/ci_scripts/osx_script.sh
@@ -7,7 +7,7 @@ brew install autoconf \
             pkgconfig \
             libtool \
             wget \
-            nim@1.6.10 \
+            nim \
             ninja \
             gnu-sed \
             coreutils \
