3897b1fbdf9bf682b0f26153e6f32a8d33e4156b
smk762 <smk762@iinet.net.au>
2023-04-01 18:06:14 +0800

    remove duplicate code

--- src/core/atomicdex/api/mm2/enable_slp_rpc.cpp
+++ src/core/atomicdex/api/mm2/enable_slp_rpc.cpp
@@ -19,7 +19,6 @@ namespace atomic_dex::mm2
 
     void from_json(const nlohmann::json& j, enable_slp_rpc_result& in)
     {
-        j.at("token_id").get_to(in.token_id);
         j.at("platform_coin").get_to(in.platform_coin);
         j.at("required_confirmations").get_to(in.required_confirmations);
         j.at("token_id").get_to(in.token_id);
