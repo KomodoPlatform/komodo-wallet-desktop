3957dcc3e5f7e28cdcb2703dcf01f4fb066384d9
cipig <cipianon@gmail.com>
2023-10-13 22:23:59 +0000

    add lcw icon

--- src/core/atomicdex/services/price/komodo_prices/komodo.prices.provider.cpp
+++ src/core/atomicdex/services/price/komodo_prices/komodo.prices.provider.cpp
@@ -141,6 +141,8 @@ namespace atomic_dex
             return "coinpaprika";
         case komodo_prices::api::provider::forex:
             return "forex";
+        case komodo_prices::api::provider::livecoinwatch:
+            return "livecoinwatch";
         default:
             return "unknown";
         }
