c5a31836d145d3e975fcbbdd1e462ee3d05bf936
smk762 <smk762@iinet.net.au>
2023-12-14 06:16:32 +0800

    update version

--- src/core/atomicdex/version/version.hpp
+++ src/core/atomicdex/version/version.hpp
@@ -21,24 +21,24 @@ namespace atomic_dex
     constexpr const char*
     get_version()
     {
-        return "0.6.1-beta";
+        return "0.7.0-beta";
     }
 
     constexpr int
     get_num_version() noexcept
     {
-        return 61;
+        return 70;
     }
 
     constexpr const char*
     get_raw_version()
     {
-        return "0.6.1";
+        return "0.7.0";
     }
 
     constexpr const char*
     get_precedent_raw_version()
     {
-        return "0.6.0";
+        return "0.6.1";
     }
 } // namespace atomic_dex
